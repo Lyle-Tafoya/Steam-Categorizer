ebe359f7f7ebc98a8601a6b88f1a4270cf23dd77
Mike Bryant <mike@mikebryant.me.uk>
2016-12-17 13:44:54 +0000

    Fix VDF parsing
    In particular:
    ```
    		"WebFav5_URL"		""
    ```

--- lib/steam/vdf.rb
+++ lib/steam/vdf.rb
@@ -8,7 +8,7 @@ module Steam
     # Parse a VDF string into a hash
     def self.parse(vdf)
       transformed_vdf = vdf.gsub(/^\t*{\n\t+"/, '{"') # Map opening
-      transformed_vdf.gsub!(/(".+")\t\t(".+")/, '\1:\2') # Key with string value
+      transformed_vdf.gsub!(/(".+")\t\t(".*")/, '\1:\2') # Key with string value
       transformed_vdf.gsub!(/(".+")\n\t*{/, '\1:{') # Key with map value
       transformed_vdf.gsub!(/(["}])\n\t*"/, '\1,"') # Key value pair followed by key value pair
       map = JSON.parse("{"+transformed_vdf+"}")
@@ -22,7 +22,7 @@ module Steam
       transformed_json = json.split("\n").to_a[1..-2].join("\n") # Remove opening and closing braces
       transformed_json.gsub!(/^\t/, '') # Remove excess indentation
       transformed_json.gsub!(/(\t*)(".+"):{/, "\\1\\2\n\\1{") # Key with map value
-      transformed_json.gsub!(/(".+"):(".+"),{,1}/, "\\1\t\t\\2") # Key with string value
+      transformed_json.gsub!(/(".+"):(".*"),{,1}/, "\\1\t\t\\2") # Key with string value
       transformed_json.gsub!(/},/, '}') # Remove trailing commas
 
       return transformed_json
